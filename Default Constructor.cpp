53ff8607fd6c8d31ad86d160a18974bea7d6a0d6
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2023-12-17 00:04:55 +0600

    C++ BSFMSTU

---
+++ Default Constructor.cpp
@@ -0,0 +1,24 @@
+//A constructor which has no parameter list or no aguments are passed through the function call is called default constructor..
+
+#include <bits/stdc++.h>
+using namespace std;
+class Line{
+ public:
+//default constructor
+ Line()
+ {
+ cout << "Hello! I am Himel";
+ }
+};
+int main(){
+//default constructor called when object is created
+ Line obj;
+return 0;
+}
+
+/*
+-------------------------
+| Himel Sarder          |
+| Dept. Of CSE, BSFMSTU |
+|------------------------
+*/
