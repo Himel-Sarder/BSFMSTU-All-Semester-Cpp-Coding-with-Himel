eb617ef2aef5a8bef44b39d629edbfc871226c01
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2023-12-16 12:31:39 +0600

    C++ BSFMSTU

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -1,15 +1,17 @@
+//The process of obtaining data members and functions from one class to another class is called inheritance.
+
 #include<bits/stdc++.h>
 
 using namespace std;
 
-class Person{
+class Person{  //parent class / Base class / Super class
     public:
         void display(){
             cout << "I am Himel" << endl;
         }
 };
 
-class student:public Person{
+class student:public Person{  //Child class / Derived class / Sub class
     public:
         void display(){
             cout << "I am a student of CSE" << endl;
@@ -21,3 +23,6 @@ int main(){
     student S;
     S.display();
 }
+
+//Himel Sarder
+//Dept. of CSE, BSFMSTU
