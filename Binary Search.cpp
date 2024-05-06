9a36abd5fed96aee7a19430695db83266239768e
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2024-05-06 18:55:08 +0600

    BSFMSTU C++

---
+++ Binary Search.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Binary search function
+int binarySearch(vector<int>& arr, int target) {
+    int left = 0;
+    int right = arr.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        // Check if target is present at mid
+        if (arr[mid] == target) {
+            return mid;
+        }
+        // If target is greater, ignore left half
+        else if (arr[mid] < target) {
+            left = mid + 1;
+        }
+        // If target is smaller, ignore right half
+        else {
+            right = mid - 1;
+        }
+    }
+
+    // If target is not found in array
+    return -1;
+}
+
+int main() {
+    vector<int> arr = {2, 3, 4, 10, 40};
+    int target = 10;
+
+    int result = binarySearch(arr, target);
+    if (result != -1) {
+        cout << "Element found at index: " << result << endl;
+    } else {
+        cout << "Element not found in array" << endl;
+    }
+
+    return 0;
+}
